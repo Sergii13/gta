3af4a5b9acbe2d161f69c8405a7afdc6e2ba050b
sergii13 <sergii.khilchuk13@gmail.com>
2023-01-11 15:18:23 +0200

    add text page

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,22 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+import { gotoBlock } from "./scroll/gotoblock.js";
+
+const priceInput = document.querySelector('#price');
+
+if (priceInput) {
+   document.addEventListener('click', (e) => {
+
+      if (e.target.closest('[data-sum]')) {
+         let target = e.target.closest('[data-sum]');
+         e.preventDefault();
+         const sum = parseInt(target.dataset.sum);
+         priceInput.parentElement.classList.add('_fill');
+         priceInput.classList.add('_fill');
+         priceInput.value = sum;
+         gotoBlock('.donate', false, 200, 0);
+
+      }
+   })
+}
